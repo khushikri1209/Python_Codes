cebee6326d15e90c8e03cd1cc678c1b7a585713e
khushikri1209 <khushikumari12029@gmail.com>
2025-05-24 23:08:55 +0530

    Update 05SplitArray.py

--- 02Arrays/05SplitArray.py
+++ 02Arrays/05SplitArray.py
@@ -22,3 +22,20 @@ for i in range(0, n):
     print(arr[i], end=' ')
 
 # Code Contributed by Mohit Gupta_OMG <(0_o)>
+
+# Python program to split array and move first
+# part to end.
+
+
+def splitArr(a, n, k):
+    b = a[:k]
+    return (a[k::]+b[::])
+
+
+# main
+arr = [12, 10, 5, 6, 52, 36]
+n = len(arr)
+position = 2
+arr = splitArr(arr, n, position)
+for i in range(0, n):
+    print(arr[i], end=' ')
