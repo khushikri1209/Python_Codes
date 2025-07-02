c95afec7a2c93528b55c01258f64a350a656c6db
khushikri1209 <khushikumari12029@gmail.com>
2025-07-02 20:19:19 +0530

    Create 05ConvertKeyValue.py

---
+++ Dictionary Programs/05ConvertKeyValue.py
@@ -0,0 +1,6 @@
+a = [("name", "Alice"), ("age", 25), ("city", "New York")]  # List of key-value pairs as tuples
+
+# Convert the list of key-value pairs into a flat dictionary using the dict() constructor
+res = dict(a)
+
+print(res)
