cc4209b757b68246c8963098e1066cb07d7ccc50
khushikri1209 <khushikumari12029@gmail.com>
2025-08-13 22:17:35 +0530

    Create 07MinSum.py

---
+++ 08Python Programs/07MinSum.py
@@ -0,0 +1,25 @@
+# Python program to find minimum
+# sum of product of number
+
+# To find minimum sum of
+# product of number
+
+def find_min_sum(num):
+    min_sum = num
+    
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            factor = num // i
+            min_sum = min(min_sum, i + factor)
+    
+    return min_sum
+
+# driver code
+number = 16
+
+# Call the function and print the result
+result = find_min_sum(number)
+print("The minimum sum of factors for", number, "is", result)
+
+
+# This code is contributed by AYUSH MILAN
